e17b870f754c726914e8d427508a24ab547c47d6
Keil Carpenter <development@keilcarpenter.pro>
2017-08-15 17:27:14 +1200

    Configure Babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,21 @@ var path = require('path');
 
 const config = {
     entry: './src/index.js',
+
+    // Configure output properties
     output: {
         path: path.resolve(__dirname, 'dist') ,
         filename: 'bundle.js'
+    },
+    module: {
+
+        // Specify loaders to use
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/  // Apply loader to .js extensions only
+            }
+        ]
     }
 };
 
